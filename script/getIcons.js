78b72a32681ddb483f8362906c78d2b9608e77a4
mainDevOps <main.devopsar@gmail.com>
2022-06-27 20:03:09 -0300

    first commit

--- script/getIcons.js
+++ script/getIcons.js
@@ -1,2 +1,12 @@
-fetch('../json/icons.json')
-.then((response) => response.json())
+const requestJSON = 'https://raw.githubusercontent.com/Niffxd/Presentation/main/json/icons.json'
+const request = new XMLHttpRequest()
+
+request.open('GET', requestJSON);
+request.responseType = 'json';
+request.send();
+
+const containerBox = document.body
+
+request.onload = function() {
+    containerBox.innerHTML += `<img src="${request.response.code[1]}">`
+} 
