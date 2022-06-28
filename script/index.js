4035c8222b535ce24ba43fa642c9339fde60902f
mainDevOps <main.devopsar@gmail.com>
2022-06-27 23:28:52 -0300

    update skills

--- script/index.js
+++ script/index.js
@@ -1,3 +1,23 @@
+const requestJSON = 'https://raw.githubusercontent.com/Niffxd/Presentation/main/json/icons.json'
+const request = new XMLHttpRequest()
+
+request.open('GET', requestJSON);
+request.responseType = 'json';
+request.send();
+
+let codeIcons = document.getElementById('code')
+let designIcons = document.getElementById('design')
+
+request.onload = function() {
+    for(let i = 0; i < request.response.code.length; i++){
+        codeIcons.innerHTML += `<img src=${request.response.code[i]}>`
+    }
+
+    for(let i = 0; i < request.response.design.length; i++){
+        designIcons.innerHTML += `<img src=${request.response.design[i]}>`
+    }
+}
+
 document.getElementById('aboutMe').addEventListener('click', () => {
     document.getElementById('aboutMe').classList.add('selected')
     document.getElementById('skills').classList.remove('selected')
